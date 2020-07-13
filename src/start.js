c19fc7d35a0423950f2d2bdfb08cb681a9c59420
Sheava <sheavad@gmail.com>
2020-07-13 16:41:24 -0700

    add title draw, will update

--- src/start.js
+++ src/start.js
@@ -13,6 +13,28 @@ class Start {
             this.ctx.fill();
         this.ctx.closePath();
     }
+
+    drawTitle() {
+        this.ctx.beginPath();
+            this.ctx.fillStyle = 'black';
+            this.ctx.font = 'bold 50px "Jua"';
+            this.textAlign = 'center';
+            this.ctx.fillText('Chonky Type!', (this.canvas.width/2), 100);
+            this.ctx.fill();
+        this.ctx.closePath();
+        
+    }
+
+    drawHeaderMessage() {
+        this.ctx.beginPath();
+            this.ctx.fillStyle = 'black';
+            this.ctx.font = '20px "Jua"';
+            this.ctx.textAlign = 'center';
+            this.ctx.fillText('Try to get the cat as fat and chonky as you can!',
+                                (this.canvas.width/2), 200);
+            this.ctx.fill();
+        this.ctx.closePath();
+    }
 }
 
 export default Start;
