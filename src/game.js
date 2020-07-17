788b30da02e7e1e152bbcba0309760583c93df98
Sheava <sheavad@gmail.com>
2020-07-17 15:28:05 -0700

    fix levels

--- src/game.js
+++ src/game.js
@@ -59,30 +59,45 @@ class Game {
         let color = colors[Math.floor(Math.random() * 2)];
         let word = this.dictionary.randomWord();
         let newFish = new Fish(this.ctx, this.canvas, word, x, y, color, this.eaten);
-        let tempFat = this.cat.fat;
-        if (this.fish.length < (tempFat + 12)) {
-            tempFat +=7
-            this.fish.push(newFish);
-            // return this.fish;
-        }
-        // this.fish = [];
-        return this.fish;
+
+        // let i = 0;
+        if (this.cat.fat === 0) {
+            if (this.fish.length < (this.cat.fat + 10)) {
+                this.fish.push(newFish);
+            }
+            return this.fish;
+
+        } else if (this.cat.fat === 5) {
+            if (this.fish.length < (this.cat.fat + 20)) {
+                this.fish.push(newFish);
+            }
+            return this.fish;
+
+        } else if (this.cat.fat === 10) {
+            if (this.fish.length < (this.cat.fat + 30)) {
+                this.fish.push(newFish);
+            }
+            return this.fish;
+
+        } else if (this.cat.fat === 15) {
+            if (this.fish.length < (this.cat.fat + 50)) {
+                this.fish.push(newFish);
+            }
+            return this.fish;
+        } 
     }
 
     addFish() {
-        let interval = 10;
+        let interval = 100;
         let now = Date.now();
         let delta = now - this.then;
-        
-        // this.fish = [];
-        let currentFish = this.populateFishArray();
 
-        currentFish.forEach( f => {
+        this.fish.forEach(f => {
             if (delta > interval) {
-                this.then = now - (delta % interval);
+                // this.then = now - (delta % interval);
                 if (f.eaten === false) {
-                    f.draw();
                     f.move();
+                    f.draw();
                     if (f.x >= 410) {
                         f.eaten = true;
                     }
@@ -90,29 +105,33 @@ class Game {
             }
         })
     }
-
+    
     handleFish(e) {
         if (e.keyCode === 32 || e.keyCode === 13) {
             let typedVal = this.input.value.trim();
             this.fish.forEach(f => {
                 if (typedVal === f.word) {
-                    this.cat.eat = true;
-                    this.cat.drawEating();
+                    // this.cat.eat = true;
+                    // this.cat.drawEating();
                     f.eaten = true;
                     f.word = null;
+                    f = null;
                     this.cat.fishEaten += 1;
-                    this.cat.eat = false;
+                    if (this.cat.fishEaten === 10 
+                        || this.cat.fishEaten === 20
+                        || this.cat.fishEaten === 30
+                        || this.cat.fishEaten === 40) {
+                        this.cat.fat += 5;
+                    };
+                    if (this.cat.fat === 20) {
+                        this.gameOver();
+                    }
+                    this.cat.draw();
+                    this.input.value = '';
+                    this.typeStart = 0;
+                    // this.cat.eat = false;
                 };
             });
-            this.cat.draw();
-            this.input.value = '';
-            this.typeStart = 0;
-
-            if (this.cat.fishEaten % 12 === 0) {
-                this.cat.fat += 5;
-                // this.populateFishArray();
-                // this.addFish();
-            };
         }
     }
 
@@ -138,33 +157,39 @@ class Game {
     render() {
         // just be responsible for putting stuff on screen
         // 
-        let req = requestAnimationFrame(this.render);
+        requestAnimationFrame(this.render);
 
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.canvas.addEventListener('click', this.input.focus());
         this.input.addEventListener('keydown', this.handleFish);
-        
-        // this.fish = [];
-        this.addFish();
 
+        this.populateFishArray();
+        let temp = this.cat.fat;
+        debugger
+        this.addFish();
+        debugger
+        if (temp !== this.cat.fat) {
+            debugger
+            this.addFish();
+        }
+    
         this.cat.drawFishEaten();
         this.cat.draw();
+
+
 
     }
 
     gameOver() {
         this.canvas.removeEventListener('click', this.input.focus());
         this.input.removeEventListener('keydown', this.handleFish);
-        this.input.removeEventListener('input', this.startTimer);
+        // this.input.removeEventListener('input', this.startTimer);
         this.input.value = '';
         this.input.disabled = true;
         this.input.style.display = 'none';
-
-        if (this.cat.fat >= 20) {
-            this.GameOver.endCounter
-            this.canvas.className = 'game-over-canvas';
-            window.overInterval = setInterval(this.gameOverAnimate, 100);
-        }
+        
+        this.GameOver.endCounter
+        this.canvas.className = 'game-over-canvas';
     }
 
     gameOverAnimate() {
