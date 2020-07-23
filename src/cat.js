98e48c86a6002f3bc4cdf9ab32c425a39cc24401
Sheava <sheavad@gmail.com>
2020-07-23 11:26:29 -0700

    relative path

--- src/cat.js
+++ src/cat.js
@@ -15,15 +15,15 @@ class Cat {
         this.cat15Img = new Image();
         this.cat15EatingImg = new Image();
         this.cat20Img = new Image();
-        this.catImg.src = '/images/cat.png';
-        this.catEatingImg.src = '/images/catEating.png';
-        this.cat5Img.src = '/images/cat5.png';
-        this.cat5EatingImg.src = '/images/cat5eating.png';
-        this.cat10Img.src = '/images/cat10.png';
-        this.cat10EatingImg.src = '/images/cat10eating.png';
-        this.cat15Img.src = '/images/cat15.png';
-        this.cat15EatingImg.src = '/images/cat15eating.png';
-        this.cat20Img.src = '/images/cat20.png';
+        this.catImg.src = '../public/images/cat.png';
+        this.catEatingImg.src = '../public/images/catEating.png';
+        this.cat5Img.src = '../public/images/cat5.png';
+        this.cat5EatingImg.src = '../public/images/cat5eating.png';
+        this.cat10Img.src = '../public/images/cat10.png';
+        this.cat10EatingImg.src = '../public/images/cat10eating.png';
+        this.cat15Img.src = '../public/images/cat15.png';
+        this.cat15EatingImg.src = '../public/images/cat15eating.png';
+        this.cat20Img.src = '../public/images/cat20.png';
     }
 
     drawEating() {
